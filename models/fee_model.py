52f388fa42766612b5c5876724368aa225712424
cookie_jar <cptpricecod098@gmail.com>
2025-06-05 22:11:21 +0530

    [demo added]

--- models/fee_model.py
+++ models/fee_model.py
@@ -1,15 +1,4 @@
 def estimate_fee(usd_amount: float, side: str = 'buy', role: str = 'taker') -> float:
-    """
-    Estimates trading fee for a given order size.
-
-    Parameters:
-        usd_amount (float): Value of the trade in USD.
-        side (str): 'buy' or 'sell' (optional here for extensibility).
-        role (str): 'maker' or 'taker'.
-
-    Returns:
-        float: Estimated fee in USD.
-    """
     if role == 'taker':
         fee_rate = 0.001  # 0.10%
     elif role == 'maker':
